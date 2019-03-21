a1f719c7fe387377cbcf44276c40ea30307d8a5c
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-20 21:03:07 -0600

    Fixed error converting base64 to image and viceversa.

--- common/models/pixelate.js
+++ common/models/pixelate.js
@@ -3,13 +3,11 @@ var Jimp = require('jimp');
 
 module.exports = function(Pixelate) {
   Pixelate.pixelate = function (image64, next) {
-    Jimp.read(Buffer.from(image64, 'base64')).then((imageReadError, image) => {
-      if(imageReadError) return next(imageReadError);
-      image.getBase64Async(mime).then((imageBase64Error, newImage64) => {
-        if(imageBase64Error) return next(imageBase64Error);
+    Jimp.read(Buffer.from(image64, 'base64')).then((image) => {
+      image.getBase64Async(Jimp.MIME_JPEG).then((newImage64) => {
         next(null, newImage64);
-      })
-    });
+      }).catch(imageBase64Error => next(imageBase64Error));
+    }).catch(imageReadError => next(imageReadError));
   }
 
   Pixelate.remoteMethod("pixelate", {
