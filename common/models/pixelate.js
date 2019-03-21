77125a2406c23525844ab588e6de4b4c243d3101
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-20 21:16:08 -0600

    As a test for pixel manipulation, pixelate method currently converts image into grayscale.

--- common/models/pixelate.js
+++ common/models/pixelate.js
@@ -4,6 +4,15 @@ var Jimp = require('jimp');
 module.exports = function(Pixelate) {
   Pixelate.pixelate = function (image64, next) {
     Jimp.read(Buffer.from(image64, 'base64')).then((image) => {
+      let width = image.bitmap.width;
+      let height = image.bitmap.height;
+      for(let x = 0; x < width; x++){
+        for(let y = 0; y < height; y++){
+          let rgb = Jimp.intToRGBA(image.getPixelColor(x, y));
+          let grayscale = (rgb.r + rgb.g + rgb.b) / 3;
+          image.setPixelColor(Jimp.rgbaToInt(grayscale, grayscale, grayscale, rgb.a), x, y);
+        }
+      }
       image.getBase64Async(Jimp.MIME_JPEG).then((newImage64) => {
         next(null, newImage64);
       }).catch(imageBase64Error => next(imageBase64Error));
