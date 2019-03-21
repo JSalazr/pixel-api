739aefcb40538d76842d0c06a0fab8693a15036e
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-20 20:51:27 -0600

    Custom method received an image as a base64 string and returns a base64 as well.

--- common/models/pixelate.js
+++ common/models/pixelate.js
@@ -1,19 +1,26 @@
 'use-strict';
+var Jimp = require('jimp');
 
 module.exports = function(Pixelate) {
-  Pixelate.pixelate = function (image, next) {
-    next();
+  Pixelate.pixelate = function (image64, next) {
+    Jimp.read(Buffer.from(image64, 'base64')).then((imageReadError, image) => {
+      if(imageReadError) return next(imageReadError);
+      image.getBase64Async(mime).then((imageBase64Error, newImage64) => {
+        if(imageBase64Error) return next(imageBase64Error);
+        next(null, newImage64);
+      })
+    });
   }
 
   Pixelate.remoteMethod("pixelate", {
       accepts: [
-          { arg: 'image', type: 'string' }
+          { arg: 'image64', type: 'string' }
       ],
       http: {
           path: '/pixelate',
           verb: 'post'
       },
       description: "Pixelates an image",
-      returns: {}
+      returns: {type: 'string', root: 'true'}
   });
 };
