f768eea4939ab27899ce89fb0f7b01239c70bc1a
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-21 19:40:57 -0600

    Pixel size is now customizable.

--- common/models/pixelate.js
+++ common/models/pixelate.js
@@ -2,63 +2,67 @@
 var Jimp = require('jimp');
 
 module.exports = function(Pixelate) {
-  Pixelate.pixelate = function (image64, next) {
-    Jimp.read(Buffer.from(image64, 'base64')).then((image) => {
-      let width = image.bitmap.width;
-      let height = image.bitmap.height;
-      let pixelSize = 5;
+  	Pixelate.pixelate = function (image64, pixelSize, next) {
+    	Jimp.read(Buffer.from(image64, 'base64')).then((image) => {
 
-      pixelate(width, height, pixelSize, image);
-      
-      image.getBase64Async(Jimp.MIME_JPEG).then((newImage64) => {
-        next(null, newImage64);
-      }).catch(imageBase64Error => next(imageBase64Error));
-    }).catch(imageReadError => next(imageReadError));
-  }
+			pixelate(pixelSize, image);
+		
+			image.getBase64Async(Jimp.MIME_JPEG).then((newImage64) => {
+				next(null, newImage64);
+			}).catch(imageBase64Error => next(imageBase64Error));
+    	}).catch(imageReadError => next(imageReadError));
+  	}
 
-  Pixelate.remoteMethod("pixelate", {
-      accepts: [
-          { arg: 'image64', type: 'string' }
-      ],
-      http: {
-          path: '/pixelate',
-          verb: 'post'
-      },
-      description: "Pixelates an image",
-      returns: {type: 'string', root: 'true'}
-  });
+	Pixelate.remoteMethod("pixelate", {
+		accepts: [
+			{ arg: 'image64', type: 'string' },
+			{ arg: 'pixelSize', type: 'number' }
+		],
+		http: {
+			path: '/pixelate',
+			verb: 'post'
+		},
+		description: "Pixelates an image",
+		returns: {type: 'string', root: 'true'}
+	});
 };
 
-function pixelate(width, height, pixelSize, image) {
-  for(let x = 0; x < width; x += pixelSize){
-    for(let y = 0; y < height; y += pixelSize){
-      let averageRGB = getAverageRGB(x, y, pixelSize, image);
-      setAverageRGB(x, y, pixelSize, averageRGB, image);
-    }
-  }
+function pixelate(pixelSize, image) {
+	let width = image.bitmap.width;
+	let height = image.bitmap.height;
+	for(let x = 0; x < width; x += pixelSize){
+    	for(let y = 0; y < height; y += pixelSize){
+      		let averageRGB = getAverageRGB(x, y, pixelSize, image);
+      		setAverageRGB(x, y, pixelSize, averageRGB, image);
+		}
+  	}
 }
 
 function getAverageRGB(startX, startY, pixelSize, image) {
-  let averageRGB = {r: 0, g: 0, b: 0, a: 0};
-  let pixelCount = 0;
+	let averageRGB = {r: 0, g: 0, b: 0, a: 0};
+	let pixelCount = 0;
 
-  for(let x = startX; x < startX + pixelSize; x++){
-    for(let y = startY; y < startY + pixelSize; y++){
-      pixelCount++;
-      let rgb = Jimp.intToRGBA(image.getPixelColor(x, y));
-      averageRGB = {r: averageRGB.r + rgb.r, g: averageRGB.g + rgb.g, b: averageRGB.b + rgb.b, a: rgb.a};
-    }
-  }
+	for(let x = startX; checkLimit(x, startX + pixelSize, image.bitmap.width); x++){
+		for(let y = startY; checkLimit(y, startY + pixelSize, image.bitmap.height); y++){
+			pixelCount++;
+			let rgb = Jimp.intToRGBA(image.getPixelColor(x, y));
+			averageRGB = {r: averageRGB.r + rgb.r, g: averageRGB.g + rgb.g, b: averageRGB.b + rgb.b, a: rgb.a};
+		}
+	}
 
-  averageRGB = {r: averageRGB.r / pixelCount, g: averageRGB.g / pixelCount, b: averageRGB.b / pixelCount, a: averageRGB.a};
+  	averageRGB = {r: averageRGB.r / pixelCount, g: averageRGB.g / pixelCount, b: averageRGB.b / pixelCount, a: averageRGB.a};
 
-  return averageRGB;
+  	return averageRGB;
 }
 
 function setAverageRGB(startX, startY, pixelSize, rgb, image) {
-  for(let x = startX; x < startX + pixelSize; x++){
-    for(let y = startY; y < startY + pixelSize; y++){
-      image.setPixelColor(Jimp.rgbaToInt(rgb.r, rgb.g, rgb.b, rgb.a), x, y);
-    }
-  }
+	for(let x = startX; checkLimit(x, startX + pixelSize, image.bitmap.width); x++){
+		for(let y = startY; checkLimit(y, startY + pixelSize, image.bitmap.height); y++){
+			image.setPixelColor(Jimp.rgbaToInt(rgb.r, rgb.g, rgb.b, rgb.a), x, y);
+		}
+	}
+}
+
+function checkLimit(pos, limit, imageLimit){
+	return pos < limit && pos < imageLimit;
 }
